30b0b0c5c4c40466bce887f475f063f4109088ab
L-Trunks <18742509891@163.com>
2021-01-07 15:28:27 +0800

    ..

--- fish_node/database/mysql.js
+++ fish_node/database/mysql.js
@@ -9,25 +9,6 @@ var pool = mysql.createPool({
     database: 'fish',
     multipleStatements: true  //是否允许执行多条sql语句
 });
-//将结果已对象数组返回
-var row = (sql, ...params) => {
-    return new Promise(function (resolve, reject) {
-        pool.getConnection(function (err, connection) {
-            if (err) {
-                reject(err);
-                return;
-            }
-            connection.query(sql, params, function (error, res) {
-                connection.release();
-                if (error) {
-                    reject(error);
-                    return;
-                }
-                resolve(res);
-            });
-        });
-    });
-};
 
 //执行代码，返回执行结果
 var execute = (sql, ...params) => {
@@ -51,6 +32,5 @@ var execute = (sql, ...params) => {
 
 //模块导出
 module.exports = {
-    row: row,
     execute: execute
 }
