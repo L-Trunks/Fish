223be5cbcbe1c8b22479af406533eb0ab7162d0d
L-Trunks <18742509891@163.com>
2021-01-21 17:12:17 +0800

    .

--- fish_node/services/articleService.js
+++ fish_node/services/articleService.js
@@ -1,8 +1,9 @@
 import mysql from '../database/mysql'
 import errorNumber from '../config/errorNum'
+
 //上传文章
 async function addArticle(params) {
-    let sql = 'insert into f_article () values ()'
+    let sql = 'insert into f_article (title,content,) values ()'
     let data = await mysql.execute(sql)
     return new Promise((resolve, reject) => {
         if (data && data.errno) {
