93c46e9b4c54b2b89424cf2391a7eee23fd408f7
Trunks <45249435+L-Trunks@users.noreply.github.com>
2021-02-20 10:44:55 +0800

    Update mysql.js

--- fish_node/database/mysql.js
+++ fish_node/database/mysql.js
@@ -3,9 +3,9 @@ const mysql = require('mysql');
 
 var pool = mysql.createPool({
     connectionLimit: 50,
-    host: '149.129.59.121',
+    host: 'localhost',
     user: 'root',
-    password: 'aliyun1998',
+    password: 'root',
     database: 'fish',
     multipleStatements: true  //是否允许执行多条sql语句
 });
